76dcaac40af6aacba736e64da2dd27dedea5f8a0
sina-tkm <torkmnsina@gmail.com>
2024-09-14 16:26:21 +0330

    Added custom hook to handle closing modal on outside click using useRef

--- src/style/ui/ConfirmDelete.jsx
+++ src/style/ui/ConfirmDelete.jsx
@@ -1,12 +1,22 @@
-function ConfirmDelete({ resourceName, onclose , disabled , onConfirm }) {
+function ConfirmDelete({ resourceName, onclose, disabled, onConfirm }) {
   return (
     <div>
       <h2 className='font-bold text-base mb-8'>{`آیا از حذف ${resourceName} مطمئن هستید؟`}</h2>
       <div className='flex justify-between items-center gap-x-16'>
-        <button className='btn btn--primary flex-1' onClick={onclose} disabled={disabled}>
+        <button
+          className='btn btn--primary flex-1'
+          onClick={onclose}
+          disabled={disabled}
+        >
           لغو{" "}
         </button>
-        <button className='btn btn--danger flex-1 py-3' disabled={disabled} onClick={onConfirm}>تایید</button>
+        <button
+          className='btn btn--danger flex-1 py-3'
+          disabled={disabled}
+          onClick={onConfirm}
+        >
+          تایید
+        </button>
       </div>
     </div>
   );
